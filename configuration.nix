393d488bf2970d9a1354f72ea60f4c00d9132324
jacob <jacobmofficial@outlook.com>
2025-01-19 01:08:37 -0500

    tx

--- configuration.nix
+++ configuration.nix
@@ -1,90 +1,91 @@
-#  sudo nixos-rebuild switch --upgrade-all
-  #  sudo nix-collect-garbage -d **try not to use** 
+# This is my current Configuration.nix File, If you're looking through it I tried to label all of the
+# more unknowed packages and also tried to sort everything neetly so it'll be easy to read.
 
-# edit this configuration file to define what should be installed on
+# sudo nixos-rebuild switch (optional) --upgrade-all
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
-# and in the nixos manual (accessible by running ‘nixos-help’).
+  #------------------------------------------------------------------------------
 
-  # Enable Prime offloading with optimus (NVIDIA Optimus laptops)
-  # We don't need a separate option here, NixOS automatically handles GPU offloading via the X server.
-  # You can use `primusrun` or `optirun` to offload to the NVIDIA GPU
-{ config, pkgs, ... }:
+  #System
+  { config, pkgs, ... }:
 
-{
+  {
+  
+  # Programs
+  programs.hyprland.enable = true;
+  programs.fish.enable = true;
+ 
+  users.defaultUserShell = pkgs.fish;
 
-nix.extraOptions = ''
- experimental-features = nix-command flakes
-'';
-
-
- programs.nix-ld.enable = true;
- programs.nix-ld.libraries = with pkgs; [
-];
-
-
-hardware.graphics = {
-    enable = true;
-  };
-
-programs.steam = {
-  enable = true;
-  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-};
-
-imports = [
+  imports =
+    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  #------------------------------------------------------------------------------
 
+  # Nvidia
+  hardware.graphics = {
+     enable = true;
+  };
 
-  # bootloader.
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+  modesetting.enable = true; # Enable modesetting for better performance
+  powerManagement.enable = false; # Disable power management to avoid interruptions
+  powerManagement.finegrained = false; # Avoid fine-grained power management
+  open = false; # Stick with proprietary driver (better for gaming)
+  nvidiaSettings = true; # Enable nvidia-settings tool for GPU management
+  package = config.boot.kernelPackages.nvidiaPackages.stable; # Stable driver package
+
+  };
+  hardware.nvidia.prime = {
+  offload = {
+  enable = true;
+  enableOffloadCmd = true;
+  };
+
+  # Make sure to use the correct Bus ID values for your system!
+  intelBusId = "PCI:0:2:0"; # Intel GPU
+  nvidiaBusId = "PCI:1:0:0"; # NVIDIA GPU
+  # amdgpuBusId = "PCI:54:0:0"; # For AMD GPUs (not applicable here)
+  };
+
+  #------------------------------------------------------------------------------
+
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # define your hostname.
-  # networking.wireless.enable = true;  # enables wireless support via wpa_supplicant.
+  #------------------------------------------------------------------------------
 
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
 
-#Nvidia
-services.xserver.videoDrivers = ["nvidia"];
+  #------------------------------------------------------------------------------
 
-hardware.nvidia = {
-modesetting.enable = true; # Enable modesetting for better performance
-powerManagement.enable = false; # Disable power management to avoid interruptions
-powerManagement.finegrained = false; # Avoid fine-grained power management
-open = false; # Stick with proprietary driver (better for gaming)
-nvidiaSettings = true; # Enable nvidia-settings tool for GPU management
-package = config.boot.kernelPackages.nvidiaPackages.stable; # Stable driver package
+  # Network
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-};
-hardware.nvidia.prime = {
-offload = {
-enable = true;
-enableOffloadCmd = true;
-};
-# Make sure to use the correct Bus ID values for your system!
-intelBusId = "PCI:0:2:0"; # Intel GPU
-nvidiaBusId = "PCI:1:0:0"; # NVIDIA GPU
-# amdgpuBusId = "PCI:54:0:0"; # For AMD GPUs (not applicable here)
-};
-
-
-  #configure network proxy if necessary
+  # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noproxy = "127.0.0.1,localhost,internal.domain";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # enable networking
+  # Enable networking
   networking.networkmanager.enable = true;
+ 
+  #------------------------------------------------------------------------------
 
-  # set your time zone.
-  time.timeZone = "america/new_york";
+  # System Settings
+  time.timeZone = "America/New_York";
 
-  # select internationalisation properties.
- i18n.defaultLocale = "en_US.UTF-8";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
@@ -97,123 +98,94 @@ nvidiaBusId = "PCI:1:0:0"; # NVIDIA GPU
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
-  # configure keymap in x11
+
+  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # define a user account. don't forget to set a password with ‘passwd’.
-    users.users.jacob = {
+  #------------------------------------------------------------------------------
+
+  # Personal (local) Packages
+  users.users.jacob = {
     isNormalUser = true;
     description = "jacob";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
+  };
 
-    };
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-  # allow unfree packages
-  nixpkgs.config.allowUnfree  = true;
+  #------------------------------------------------------------------------------
 
-# list packages installed in system profile. to search, run:
-  # $ nix search wget
-  
+  # System (global) Packages
   environment.systemPackages = with pkgs; [
-  #  vim # do not forget to add an editor to edit configuration.nix! the nano editor is also installed by default.
 
   #code
   neovim
-  gh
   git
+  gh
   wget
   gcc
-  nodejs_23
-  unzip
+  deno
   python312
   pkg-config
-  deno
-  wl-clipboard
-  luarocks
-  stylua
-  fzf
-  picom
-  obsidian
-  python312Packages.pip
-  python312Packages.cmake
-  gnumake42
+  luarocks #(Lazyvim)
+  stylua #(LazyVim)
+  fzf #Fuzzy Finder (LazyVim)
+  picom #Compositor
   libgccjit
+  nodejs_22
+  sublime4
 
-#hyprland 
-  kitty
-  tofi
-  waybar
-  killall
-  brightnessctl
-  grim # helper for screenshots within hyprland
-  slurp
-  playerctl 
-  networkmanagerapplet 
-  dunst
-  fish
-  xdg-utils
+  #System
+  kitty 
+  fish 
+  brightnessctl #Controls Brightness
+  unzip # Unzip packages
+  wl-clipboard # For automatically copying whole files  
+  tofi # Search tool
+  flameshot # Screenshot Tool
+  playerctl # Music Player for keys
+  dunst # Notification daemon
+  xdg-utils # command-line utilites
   fastfetch
   hyprpaper
   wayland
   wayland-protocols
-  mesa
+  mesa #Collection of graphic libraries that run OpenGL, Vulkan, ect.
   vulkan-tools
-  wineWowPackages.waylandFull
-  perl540Packages.OpenGL
-  usbutils
-  cheese
-  v4l-utils
-  libv4l
-  jellyfin-ffmpeg
-  gopro
-  zenith-nvidia
-  btop
-  sxiv
-  wakatime
+  wineWowPackages.waylandFull #For running window applications on Linux
+  usbutils #Collection of tools for interacting with USB devices
+  btop # Resource monitoring tool 
+  gthumb # Image viewer
+  glib # Low level core library (used by many Linux applications)
+  gtk3 # To create window application (vauge)
+  nvtopPackages.nvidia # For Nvidia
   openssl
-  wakatime-cli
-  gthumb
-  glib
-  gtk3
+  gparted # Graphical disk paritioning tool
 
-  #personal applications
-  vesktop
+  #personal
+  obsidian
   google-chrome
+  vesktop 
   lunar-client
   obs-studio
-  steam 
-  heroic
   vlc
   spotify
-  skypeforlinux
+  ];
 
-  #Nvidia
-  nvtopPackages.nvidia
-];
+  #------------------------------------------------------------------------------
+
+  # Other
 
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "JetBrainsMono" "CascadiaCode" ]; })
   ];
 
-  #hyprland
-  programs = {
-    hyprland = {
-      enable = true;
-      xwayland.enable = true;
-    };
-    waybar.enable = true;
-    xwayland.enable = true;
-    gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-    };
-};
-
-  # Set cursor theme and size globally for XWayland and Wayland apps
+# Set cursor theme and size globally for XWayland and Wayland apps
   environment.variables = {
     XCURSOR_THEME = "HyprBibataModernClassicSVG";  # Cursor theme for XWayland apps
     XCURSOR_SIZE = "24";  # Cursor size for XWayland apps
@@ -221,32 +193,39 @@ nvidiaBusId = "PCI:1:0:0"; # NVIDIA GPU
     HYPRCURSOR_SIZE = "24";  # Cursor size for Wayland apps (Hyprland)
   };
 
-
-# some programs need suid wrappers, can be configured further or are
+ 
+  nixpkgs.config.permittedInsecurePackages = [
+    "openssl-1.1.1w"
+  ];
+  
+  # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
-  #   enablesshsupport = true;
+  #   enableSSHSupport = true;
   # };
 
-  # list services that you want to enable:
+  # List services that you want to enable:
 
-  # enable the openssh daemon.
+  # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # open ports in the firewall.
-  # networking.firewall.allowedtcpports = [ ... ];
-  # networking.firewall.allowedudpports = [ ... ];
-  # or disable the firewall altogether.
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # this value determines the nixos release from which the default
+  # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. it‘s perfectly fine and recommended to leave
+  # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
-  # before changing this value read the documentation for this option
+  # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }
+
+  #------------------------------------------------------------------------------
+
